da15d5e03fd0d138b4bb92d66f24f5d38e5f4557
Serhii Nykyforov <04mailbox@gmail.com>
2017-09-16 19:28:34 +0300

    Add firewall_ssh, app_ip resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,8 +19,11 @@ resource "google_compute_instance" "app" {
   }
 
   network_interface {
-    network       = "default"
-    access_config = {}
+    network = "default"
+
+    access_config = {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
   }
 
   tags = ["reddit-app"]
@@ -53,4 +56,21 @@ resource "google_compute_firewall" "firewall_puma" {
 
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
+}
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name        = "default-allow-ssh"
+  description = "Allow SSH from anywhere"
+  network     = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
 }
